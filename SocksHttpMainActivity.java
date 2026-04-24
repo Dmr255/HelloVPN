53a8dc84a65147b23635694c54fac2fbdea4a30a
Dmr255 <110240699+Dmr255@users.noreply.github.com>
2026-04-24 21:47:28 +0700

    Updated import for SMS_Updater and usage

---
+++ SocksHttpMainActivity.java
@@ -0,0 +1,5 @@
+// Your updated content should include the necessary changes for the class
+// ...[other parts of the file]
+import com.slipkprojects.sockshttp.activities.SMS_Updater;
+// ...[other parts of the file]
+new SMS_Updater(this);
